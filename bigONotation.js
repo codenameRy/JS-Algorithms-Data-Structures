55f9055a77932d9387e9e1d6e8b99d18bf1ea070
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-14 22:27:35 -0400

    Updates

--- bigONotation.js
+++ bigONotation.js
@@ -50,6 +50,7 @@ console.log(`Time Elapsed: ${(t5 - t4) / 1000} seconds.`)
 
 
 //Print All Pairs
+//O(n^2) EXAMPLE
 
 function printAllPairs(n) {
   for (var i = 0; i < n; i++) {
@@ -63,3 +64,60 @@ let t6 = performance.now();
 console.log(printAllPairs(10));
 let t7 = performance.now();
 console.log(`Time Elapsed: ${(t7 - t6) / 1000} seconds.`)
+
+//O(n) EXAMPLE
+function logAtLeast5(n) {
+  for (var i = 1; i <= Math.max(5, n); i++) {
+    console.log(i);
+  }
+}
+
+let t41 = performance.now();
+console.log(logAtLeast5(10));
+let t42 = performance.now();
+console.log(`Time Elapsed: ${(t42 - t41) / 1000} seconds.`)
+
+//O(1) EXAMPLE
+function logAtMost5x(n) {
+  for (var i = 1; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
+}
+
+let t31 = performance.now();
+console.log(logAtMost5x(12));
+let t32 = performance.now();
+console.log(`Time Elapsed: ${(t32 - t31) / 1000} seconds.`)
+
+//Space Complexity
+
+//Sum Function
+// O(1) SPACE EXAMPLE
+function sum(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+let p1 = performance.now();
+console.log(sum([2,3,4]));
+let p2 = performance.now();
+console.log(`Time Elapsed: ${(p2 - p1) / 1000} seconds.`)
+
+//Double Function
+
+//O(n) SPACE EXAMPLE
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+}
+
+let a1 = performance.now();
+console.log(double([2,3,4,2]));
+let a2 = performance.now();
+console.log(`Time Elapsed: ${(a2 - a1) / 1000} seconds.`)
