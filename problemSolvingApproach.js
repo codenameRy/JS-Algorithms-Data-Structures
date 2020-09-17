2c5bb821cea58b6a26fa1bdea2284e596d561fdd
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-16 23:03:48 -0400

    Updates

--- problemSolvingApproach.js
+++ problemSolvingApproach.js
@@ -1,7 +1,7 @@
 const {performance} = require('perf_hooks');
 
 //Understand the problem
-//Function that takes two numbers and return their sum
+//1 - Function that takes two numbers and return their sum
 
 //O(1) Example
 function sumTwo(a, b) {
@@ -51,7 +51,7 @@ function stringCount(str) {
 
 console.log(stringCount('House'));
 
-//Function that takes a string and returns a count of each character in a string
+//2 - Function that takes a string and returns a count of each character in a string
 
 //Pseudo Code
 
@@ -79,13 +79,59 @@ function charCount1(str) {
 return result;
 }
 
-console.log(charCount1("hello"))
+//Performance Test
+let t20 = performance.now();
+// console.log(sumTwo(102, 9868));
+console.log(charCount1("towersmang"));
+let t21 = performance.now();
+console.log(`Time Elapsed: ${(t21 - t20) / 1000} seconds.`)
+// console.log(charCount1("hello"))
 
 
 //Solution 1 Refactored
+function charCount(str) {
+    let result = {};
+    // for (let i = 0; i < str.length; i++) {
+        for (let char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            if (result[char] > 0) {
+                result[char]++;
+            } else {
+                result[char] = 1;
+                } 
+        }
+    }
+    return result;
+}
 
+//Performance Test
+let t10 = performance.now();
+// console.log(sumTwo(102, 9868));
+console.log(charCount("towersmang"));
+let t11 = performance.now();
+console.log(`Time Elapsed: ${(t11 - t10) / 1000} seconds.`)
 
-//Solution 2
+//Solution 1 Refactored V2 Regular Expression
+function charCount3(str) {
+    let result = {};
+        for (let char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            result[char] = result[char]++ || 1;
+        }
+    }
+    return result;
+}
+
+//Performance Test
+let t40 = performance.now();
+// console.log(sumTwo(102, 9868));
+console.log(charCount3("towersmang"));
+let t41 = performance.now();
+console.log(`Time Elapsed: ${(t41 - t40) / 1000} seconds.`)
+
+//Solution 2 
 function charCount2(str) {
     return str.toLowerCase().split('').reduce((acc, char) => {
       acc[char] = (acc[char] || 0) + 1;
@@ -93,4 +139,8 @@ function charCount2(str) {
     }, {});
   }
 
-  console.log(charCount2('House'));
+//Performance Test
+let t30 = performance.now();
+console.log(charCount2("towersmang"));
+let t31 = performance.now();
+console.log(`Time Elapsed: ${(t31 - t30) / 1000} seconds.`)
