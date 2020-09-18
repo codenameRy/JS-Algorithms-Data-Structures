e95ae41003213d4410384b76d9acb4d8f267109b
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-18 19:57:43 -0400

    Multiple Pointer problem #1

--- problemSolvingPatterns.js
+++ problemSolvingPatterns.js
@@ -156,3 +156,56 @@ let t6 = performance.now();
 console.log(`Anagram check #2 ${anagramChecker(word1,word2)}`);
 let t7 = performance.now();
 console.log(`Time Elapsed: ${(t7 - t6) / 1000} seconds.`)
+
+//Multiple Pointers
+
+/*Write a function called sumZero which accepts a sorted array of 
+integers. The function should find the first pair where the sum is 0. 
+Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+Examples
+sumZero([-3,-2,-1,0,1,2,3]) // [-3,3] 
+sumZero([-2,0,1,3]) // undefined
+sumZero([1,2,3]) // undefined
+*/
+
+//Solution 1 - Time Complexity O(n^2) / Space Complexity O(1)
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+//Performance Test
+let t10 = performance.now();
+console.log(sumZero([-3,-2,-1,0,1,2,3]));
+let t11 = performance.now();
+console.log(`Time Elapsed: ${(t11 - t10) / 1000} seconds.`)
+
+//Solution 2 - Time Complexity O(n) / Space Complexity O(1)
+function sumZeroNew(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        } else if(sum > 0){
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+//Performance Test
+let t12 = performance.now();
+// console.log(sumZeroNew([-3,-2,-1,0,1,2,3,10])); // [-3,3]
+console.log(sumZeroNew([-3,-2,-1,0,5,10])); //Undefined
+let t13 = performance.now();
+console.log(`Time Elapsed: ${(t13 - t12) / 1000} seconds.`)
+
