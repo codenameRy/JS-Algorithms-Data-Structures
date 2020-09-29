7f730ab99f4ab19f3075c3bca789a7dad49550a2
Ryan Lee <ryanm.lee11@gmail.com>
2020-09-29 12:33:16 -0400

    Product of Array and Range Sum recursive problems

--- recursionProblemSet1.js
+++ recursionProblemSet1.js
@@ -1,5 +1,5 @@
 const { performance } = require('perf_hooks');
-//Power
+//1 - Power
 
 function powerN(base,exp) {
     if (exp === 0) return 1;
@@ -37,7 +37,7 @@ console.log(`Power exponential #3 is ${(powerN3(3,100))}`);
 let t6 = performance.now();
 console.log(`Time Elapsed: ${(t6 - t5) / 1000} seconds.`)
 
-//Factorial
+//2 - Factorial
 
 //Solution 1 - Recursion
 
@@ -51,3 +51,62 @@ let t7 = performance.now();
 console.log(`Factorial #1 is ${(factorial(4))}`);
 let t8 = performance.now();
 console.log(`Time Elapsed: ${(t8 - t7) / 1000} seconds.`)
+
+//3 - Product of Array
+
+/*
+roductOfArray
+Write a function called productOfArray which takes in an 
+array of numbers and returns the product of them all.
+*/
+
+//Solution 1 - Recursion
+function productOfArray(num){
+    if (num.length === 0) return 1;
+    return num.pop() * productOfArray(num)
+}
+
+let prodArr1 = [1,2,3,4,8,100];
+
+//Performance
+let t9 = performance.now();
+console.log(`Product of array Recursion #1 is ${productOfArray(prodArr1)}`);
+let t10 = performance.now();
+console.log(`Time Elapsed: ${(t10 - t9) / 1000} seconds.`)
+
+//Solution 2 - Iterative
+function productOfArrayNew(num){
+    let product = 1;
+    let numLength = num.length;
+    for (let i = 0; i < numLength; i++) {
+        product *= num[i];
+    }
+    return product;
+}
+
+let prodArr = [1,2,3,4,8,100];
+
+//Performance
+let t11 = performance.now();
+console.log(`Product of array Iterative #2 is ${productOfArrayNew(prodArr)}`);
+let t12 = performance.now();
+console.log(`Time Elapsed: ${(t12 - t11) / 1000} seconds.`)
+
+//4 - Recursive Sum Range
+
+/*
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+*/
+
+function recursiveRange(num){
+    if (num === 1) return 1;
+    return num + recursiveRange(num - 1)
+ }
+
+ //Performance
+let t13 = performance.now();
+console.log(`Sum range Recursive #1 is ${productOfArrayNew(6)}`);
+let t14 = performance.now();
+console.log(`Time Elapsed: ${(t14 - t13) / 1000} seconds.`)
