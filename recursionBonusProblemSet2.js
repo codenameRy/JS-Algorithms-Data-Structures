9ad4cedb4343395f85f095a7afff173afcb2cf97
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-02 23:01:12 -0400

    Recursion with callback problem and solution

--- recursionBonusProblemSet2.js
+++ recursionBonusProblemSet2.js
@@ -34,7 +34,7 @@ console.log(`Reverse Built-In Method #2 is ${reverseNew("paradisezoohouse")}`);
 let t4 = performance.now();
 console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`)
 
-//Solution 2 - Iterative with Incremental for Loop
+//Solution 3 - Iterative with Incremental for Loop
 function reverseNew2(arr){
     let revString = '';
     for (let i = 0; i <= arr.length - 1; i++) {
@@ -88,9 +88,52 @@ let t9 = performance.now();
 console.log(`Palindrom Iterative #1 is ${isPalindrome("racecar")}`);
 let t10 = performance.now();
 console.log(`Time Elapsed: ${(t10 - t9) / 1000} seconds.`)
-// console.log(isPalindrome("racecar"))
 
 //3 - Recursive
+/*
+Write a recursive function called someRecursive which accepts an array 
+and a callback. The function returns true if a single value in the array 
+returns true when passed to the callback. Otherwise it returns false.
+
+
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+*/
+
+//Solution 1 - Recursion
+function someRecursive(arr, callback) {
+    if (arr.length === 0) return false;
+    if (callback(arr[0])) return true;
+    else arr.splice(0,1); return someRecursive(arr, callback)
+}
+
+const isOdd = val => val % 2 !== 0;
+//Performance
+let t11 = performance.now();
+console.log(`Recursive Odd Check #1 is ${someRecursive([12,2,6,4], isOdd)}`);
+let t12 = performance.now();
+console.log(`Time Elapsed: ${(t12 - t11) / 1000} seconds.`)
+
+//Solution 2 - Recursion Refined solution
+function someRecursiveNew(arr, callback) {
+    if (arr.length === 0) return false;
+    if (callback(arr[0])) return true;
+    // else arr.splice(0,1); return someRecursive(arr, callback)
+    return someRecursiveNew(arr.slice(1), callback)
+}
+
+const isOdd2 = val => val % 2 !== 0;
+//Performance
+let t13 = performance.now();
+console.log(`Recursive Odd Check #1 is ${someRecursiveNew([1,2,6,4], isOdd2)}`);
+let t14 = performance.now();
+console.log(`Time Elapsed: ${(t14 - t13) / 1000} seconds.`)
+
 
 //4 - Flatten
 
