455cbba9176f649f8e9734d0355c68374884acd9
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-19 02:12:02 -0400

    Binary Search While Loop solution

--- searchingAlgorithms/binarySearch.js
+++ searchingAlgorithms/binarySearch.js
@@ -36,4 +36,29 @@ console.log(numSearch(numSearchArr,20));
 let t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
 
+//Solution 2 - While Loop
+function binarySearch(arr,val) {
+    let left = 0;
+    let right = arr.length - 1;
+    let middle = (left + right)/2;
+
+    while(arr[middle] !== val && left <= right) {
+        if (val < arr[middle]) right = middle - 1;
+        else start = middle + 1;
+        middle = (left + right)/2;
+
+    }
+    // if (arr[middle] === val) return middle;
+    // return -1;
+    return arr[middle] === val ? middle : -1;
+}
+
+numSearchArr3 = [ 1, 3, 4, 6, 8, 9, 11, 12, 15, 16, 17, 18, 19, 20 ];
+
+//Performance Test
+let t3 = performance.now();
+console.log(binarySearch(numSearchArr3,20));
+let t4 = performance.now();
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`)
+
 
