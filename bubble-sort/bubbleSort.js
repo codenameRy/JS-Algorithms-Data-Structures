6e06de97915aa28fc63036a73a8796c606df2e86
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-26 16:54:59 -0400

    Bubble Sort optimized solutions

--- bubble-sort/bubbleSort.js
+++ bubble-sort/bubbleSort.js
@@ -15,7 +15,7 @@ function swap(arr, indx1, indx2) {
 //     [arr[indx1],arr[indx2]] = [arr[indx2],arr[indx1]];
 // }
 
-//Solution 1 -  Naive (Not Optimized)
+//Solution 1 Naive (Not Optimized) - - Time Complexity O(N)
 function bubbleSortNaive(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (j = 0; j < arr.length; j++) {
@@ -30,16 +30,13 @@ function bubbleSortNaive(arr) {
     return arr;
 }
 
-
-numArr = [2,9,10,1];
-
 //Performance Test
 let t1 = performance.now();
-console.log(`Bubble Sort Naive ${bubbleSortNaive(numArr)}`);
+console.log(`Bubble Sort Naive ${bubbleSortNaive([2,9,10,1])}`);
 let t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
 
-//Solution 2 Optimized
+//Solution 2 Decrementing - Time Complexity O(N)2
 function bubbleSort(arr) {
     //Start looping from with a variable called i the end of the array towards the beginning
     for (let i = arr.length; i > 0; i--) {
@@ -56,11 +53,57 @@ function bubbleSort(arr) {
     return arr;
 }
 
-numArr2 = [2,9,10,1];
-
 //Performance Test
 let t3 = performance.now();
-console.log(`Bubble Sort New ${bubbleSort(numArr2)}`);
+console.log(`Bubble Sort New ${bubbleSort([2,9,10,1])}`);
 let t4 = performance.now();
 console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`)
+
+//Solution 3 ES6 Decrementing - Time Complexity O(N2)
+function bubbleSortES6(arr) {
+    const swap = (arr, indx1, indx2) => {
+        [arr[indx1],arr[indx2]] = [arr[indx2],arr[indx1]];
+    }
+    //Start looping from with a variable called i the end of the array towards the beginning
+    for (let i = arr.length; i > 0; i--) {
+        //Start an inner loop with a variable called j from the beginning until i - 1
+        for (let j = 0; j < i - 1; j++) {
+            //If arr[j] is greater than arr[j+1], swap those two values!
+            if (arr[j] > arr[j+1]) {
+                swap(arr,j,j+1)
+            }
+        }
+    }
+    return arr;
+}
+
+//Performance Test
+let t5 = performance.now();
+console.log(`Bubble Sort ES6 ${bubbleSortES6([2,9,10,1])}`);
+let t6 = performance.now();
+console.log(`Time Elapsed: ${(t6 - t5) / 1000} seconds.`)
+
+//Solution 4 - Optimized to check for No swaps - Time Complexity O(N)
+function bubbleSortLatest(arr) {
+    let noSwaps;
+    for (let i = arr.length; i < 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
+
+//Performance Test
+let t7 = performance.now();
+console.log(`Bubble Sort No Swaps Check ${bubbleSortLatest([8,1,2,3,4,5,6,7])}`);
+let t8 = performance.now();
+console.log(`Time Elapsed: ${(t8 - t7) / 1000} seconds.`)
 
