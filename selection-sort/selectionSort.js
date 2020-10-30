dcc456b7be137647663a2004a15d76d9811b62c0
Ryan Lee <ryanm.lee11@gmail.com>
2020-10-30 17:54:21 -0400

    Selection Sort ES6 solution

--- selection-sort/selectionSort.js
+++ selection-sort/selectionSort.js
@@ -36,3 +36,27 @@ let t1 = performance.now();
 console.log(`Bubble Sort Naive ${selectionSort([2,9,10,1])}`);
 let t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+//Solution 2 
+function selectionSortNew(arr) {
+    const swap = (arr, idx1, idx2) =>
+      ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+  
+    for (let i = 0; i < arr.length; i++) {
+      let lowest = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[lowest] > arr[j]) {
+          lowest = j;
+        }
+      }
+      if (i !== lowest) swap(arr, i, lowest);
+    }
+  
+    return arr;
+  }
+
+  //Performance Test
+let t3 = performance.now();
+console.log(`Bubble Sort #2 ${selectionSortNew([2,9,10,1])}`);
+let t4 = performance.now();
+console.log(`Time Elapsed: ${(t4 - t3) / 1000} seconds.`)
