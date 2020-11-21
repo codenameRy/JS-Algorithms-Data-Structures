6ebc6596e48f38c005305f8a240c3fce7c09f832
Ryan Lee <ryanm.lee11@gmail.com>
2020-11-21 17:23:41 -0500

    Pivot Helper Function updates

--- quick-sort/pivotHelperFunction.js
+++ quick-sort/pivotHelperFunction.js
@@ -27,3 +27,54 @@ and then swap the current element with the element at the pivot index
 Swap the starting element (i.e. the pivot) with the pivot index
 Return the pivot index
 */
+
+// First Version
+function pivot(arr, start=0, end=arr.length+1){
+    function swap(array, i, j) {
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  
+    var pivot = arr[start];
+    var swapIdx = start;
+  
+    for(var i = start + 1; i < arr.length; i++){
+      if(pivot > arr[i]){
+        swapIdx++;
+        swap(arr,swapIdx,i);
+      }
+    }
+    swap(arr,start,swapIdx);
+    return swapIdx;
+  }
+  
+  // Version with ES2015 Syntax
+  function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+  
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+  
+    for (let i = start + 1; i <= end; i++) {
+      if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+      }
+    }
+  
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx);
+    return swapIdx;
+  }
+  
+//   pivot([4,8,2,1,5,7,6,3])
+
+  //Performance Test
+let t1 = performance.now();
+console.log(`Pivot Helper Function ${pivot([1,50,10, 104, 2,14,99, 101])}`);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
