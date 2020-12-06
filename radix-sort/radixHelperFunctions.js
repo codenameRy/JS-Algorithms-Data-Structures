ee4630552aba1cee51d7f100bd3f1aad081085e8
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-06 17:55:01 -0500

    Radix Sort initial update

--- radix-sort/radixHelperFunctions.js
+++ radix-sort/radixHelperFunctions.js
@@ -9,8 +9,10 @@ function getDigit(num, i) {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
 
-console.log(`Get Digit ${getDigit(7234, 0)}`)
+console.log(`Get Digit ${getDigit(7234, 1)}`)
 
+//digitCount(num) - returns the number of digits in num
+//10 to power of n 
 function digitCount(num) {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
@@ -18,12 +20,17 @@ function digitCount(num) {
 
 console.log(`Digit Count ${digitCount(7234)}`)
 
+//mostDigits(nums) - Given an array of numbers, returns the number of 
+//digits in the largest numbers in the list
+
 function mostDigits(nums) {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
+
+    //Setting maxDigits as the largest number between the current value of maxDigits and the digitCount of nums[i]
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
   return maxDigits;
 }
 
-console.log(mostDigits([23,567,890,12234324,900,720]))
+console.log(`Most Digits in the data set ${mostDigits([23,567,890,12234324,900,720])}`)
--- radix-sort/radixSort.js
+++ radix-sort/radixSort.js
@@ -1,1 +1,51 @@
 const { performance } = require('perf_hooks');
+
+//Radix Sort Pseudocode
+
+function getDigit(num, i) {
+    //Absolute Value for positive or negative numbers
+    //Divide by 10 by the power of i
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+  }
+  
+//   console.log(`Get Digit ${getDigit(7234, 12)}`)
+  
+  //digitCount(num) - returns the number of digits in num
+  //10 to power of n 
+  function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+  
+//   console.log(`Digit Count ${digitCount(7234)}`)
+  
+  //mostDigits(nums) - Given an array of numbers, returns the number of 
+  //digits in the largest numbers in the list
+  
+  function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+  
+      //Setting maxDigits as the largest number between the current value of maxDigits and the digitCount of nums[i]
+      maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+  }
+
+  //Solution 1
+
+  function radixSort(nums){
+    let maxDigitCount = mostDigits(nums);
+    for (let k = 0; k < nums.length; k++) {
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k)
+            digitBuckets[digit].push(nums[i]);
+        }
+        console.log(digitBuckets)
+    }
+  }
+
+
+
+  console.log(`Radix Sort ${radixSort([280,42, 8421, 200383, 12,102])}`)
