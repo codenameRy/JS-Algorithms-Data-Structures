490d12a1bac639c93482ab6bdad41a62011ee274
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-07 14:18:31 -0500

    Updates

--- radix-sort/radixSort.js
+++ radix-sort/radixSort.js
@@ -33,6 +33,14 @@ function getDigit(num, i) {
   }
 
   //Solution 1
+/*Define a function that accepts list of numbers
+Figure out how many digits the largest number has
+Loop from k = 0 up to this largest number of digits
+For each iteration of the loop:
+Create buckets for each digit (0 to 9)
+place each number in the corresponding bucket based on its kth digit
+Replace our existing array with values in our buckets, starting with 0 and going up to 9
+return list at the end!*/
 
   function radixSort(nums){
     let maxDigitCount = mostDigits(nums);
