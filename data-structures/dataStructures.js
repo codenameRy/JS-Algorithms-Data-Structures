252ffaaf3ea96fbcd59413ae6812c6015fd793bc
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-18 22:15:05 -0500

    Performance update

--- data-structures/dataStructures.js
+++ data-structures/dataStructures.js
@@ -1,1 +1,7 @@
 const { performance } = require('perf_hooks');
+
+//   //Performance Test
+let t1 = performance.now();
+console.log();
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
