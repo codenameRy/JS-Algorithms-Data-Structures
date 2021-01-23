257729ffddb072b879c4359fb6b7698fc5bbf30f
Ryan Lee <ryanm.lee11@gmail.com>
2021-01-22 22:07:01 -0500

    Data Structures update

--- data-structures/dataStructures.js
+++ data-structures/dataStructures.js
@@ -1,7 +1,7 @@
 const { performance } = require('perf_hooks');
 
 //   //Performance Test
-let t1 = performance.now();
-console.log();
 let t2 = performance.now();
+console.log();
+let t3 = performance.now();
 console.log(`Time Elapsed: ${(t3 - t2) / 1000} seconds.`)
